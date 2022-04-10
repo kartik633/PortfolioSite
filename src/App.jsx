ad1b76163d269310314df5a59c6b08412a651df9
Kartik <9415kks@gmail.com>
2022-04-10 23:19:35 +0530

    Adding the components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Topbar from "./components/topbar/Topbar";
+
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar/>
+      <div className="sections">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
